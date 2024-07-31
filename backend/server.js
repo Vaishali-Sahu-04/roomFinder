0aa51e2db8f9b9800a937af371d51157abe04a4b
Vaishali-Sahu-04 <vaishusahuone@gmail.com>
2024-07-31 23:17:55 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import path from 'path'
 
 import userRouter from './routes/user.route.js'
 import roomRouter from './routes/room.router.js'
@@ -12,10 +13,11 @@ import { connect } from './db/connect.js';
 const app=express();
 dotenv.config();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve()
 
 app.use(cors({
-     origin: 'http://localhost:5173',
+     origin: 'http://localhost:3000',
      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
      credentials: true, // if you need to handle cookies
      allowedHeaders: 'Content-Type,Authorization',}
@@ -27,12 +29,15 @@ app.use(cookieParser())
 app.use( express.static('public'));
 
 
-app.get("/", (req,res)=>{
-    res.json("Hello from server")
-})
 app.use("/api/users",userRouter);
 app.use("/api/rooms",roomRouter);
 app.use("/api/reviews",reviewRouter);
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*",(req,res)=>{
+	res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+});
 
 connect();
 app.listen(PORT,(()=>console.log("Server started on PORT",PORT)))
