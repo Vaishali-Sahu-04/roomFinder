7ada8de58dc03014145815d80eb0a806e62bce0f
Vaishali-Sahu-04 <vaishusahuone@gmail.com>
2024-08-06 22:03:22 +0530

    modified vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',  // Ensure this matches the directory Vercel expects
+  },
   server: {
     open: true,
     hmr: true,
