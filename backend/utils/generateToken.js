095157946695f5181723abcd07dd20307a767439
Vaishali-Sahu-04 <vaishusahuone@gmail.com>
2024-08-08 00:34:20 +0530

    .

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken'
 const generateTokenAndSetCookie = (userId, res) => {
     const token = jwt.sign({userId}, process.env.JWT_SECRET, { expiresIn: '3h' })
 
-    res.cookie("jwt",token,{
+    return res.cookie("jwt",token,{
         httpOnly: true,
         //sameSite: "strict",
         // secure: process.env.NODE_ENV !== "development"
