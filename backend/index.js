591adffbc9ba8f43dd6f1f4243ba203976fa6cef
Vaishali-Sahu-04 <vaishusahuone@gmail.com>
2024-08-07 20:19:52 +0530

    added deployed url

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ console.log(__dirname);
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://frontend-room-finder.vercel.app",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true, // if you need to handle cookies
     allowedHeaders: "Content-Type,Authorization",
